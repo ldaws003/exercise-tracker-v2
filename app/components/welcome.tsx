38123d12fbd4e0fee44959808446b5f77bd6bbb8
ldaws003 <leondawson123@yahoo.com>
2026-05-24 19:04:33 -0400

    created welcome page for unsigned in users

---
+++ app/components/welcome.tsx
@@ -0,0 +1,11 @@
+export default function Welcome(){
+    return (
+        <div>
+            <h2>Hi Welcome to the Exercise Tracker V2</h2>
+            <p>This is a personal project showcasing Chart.js, a SQL database, and OAuth. 
+                Sign in with your google account and be taken to a dashboard where you can keep track of your
+                exercises and the amount of calories you burned.</p>
+            <p>Sign in using the link on the top right corner</p>
+        </div>
+    )
+}
