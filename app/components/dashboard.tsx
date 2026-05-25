61ff7fb1495afb9bb86f1dae9a7021c6f381eb42
ldaws003 <leondawson123@yahoo.com>
2026-05-25 04:15:57 -0400

    created dummy data

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -1,7 +1,22 @@
+
+
+// dummy data to use for now
+let dummy_data: any = {
+    cal_data: [300, 500, 200, 500, 10, 56, 327],
+    exercises: [
+        {activity: "running", time: "300s"},
+        {activity: "push ups", time: "200s"},
+        {activity: "running", time: "300s"},
+        {activity: "biking", time: "700s"},
+        {activity: "running", time: "300s"},
+    ],
+}
+
 export default function Dashboard(){
     
     return (
     <div>
+        <div></div>
 
     </div>
     )
