7527e6e4594b4b87c1024a0c605a8dcb5e4dfe7b
ldaws003 <leondawson123@yahoo.com>
2026-05-26 19:16:48 -0400

    changed todo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { SessionProvider } from "next-auth/react"
 import HomePage from "./home/page";
 
-// TODO: make default home page (user didn't sign in)
+// TODO? make sign in form
 // TODO: make db to save exercise data
 // TODO: decide which sql to use to fetch and load data
 
