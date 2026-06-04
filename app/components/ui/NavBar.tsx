4c810edefd6d01c6d58583d8d5d5f23a29e39b8c
ldaws003 <leondawson123@yahoo.com>
2026-06-03 22:31:00 -0400

    moved navbar into it's own component

--- app/components/ui/NavBar.tsx
+++ app/components/ui/NavBar.tsx
@@ -33,7 +33,7 @@ export default async function NavBar(){
                 {
                     session?.user ? (
                         <div>
-
+                          <SignOut />
                         </div>
                     ) : (
                     <Link href="/api/auth/signin">
