9fe0dd425bc208d935a7304ce2b03c1a257b3517
ldaws003 <leondawson123@yahoo.com>
2026-06-04 17:21:02 -0400

    created about page

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+// TODO fill with actual text to be used
+
+export default function About(){
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus aliquam viverra. Cras eget ex nec ligula 
+                porttitor tincidunt. Duis rhoncus diam imperdiet odio ullamcorper, eu imperdiet libero scelerisque. Cras 
+                efficitur placerat pulvinar. Donec sed libero a dolor feugiat feugiat. Vivamus gravida ligula quis arcu bibendum
+                 molestie. Quisque pharetra, ligula eget condimentum hendrerit, leo tortor luctus mi, quis fringilla turpis 
+                 ligula id libero. Pellentesque quis dapibus quam. In scelerisque nibh ac malesuada malesuada. Cras pellentesque
+                  eros a tortor venenatis, vitae venenatis purus feugiat. Morbi eu scelerisque purus. Fusce nec tellus vitae 
+                  tortor pharetra finibus at nec diam. Pellentesque bibendum leo sed neque tincidunt, a lacinia nibh dictum. 
+                  Mauris volutpat auctor arcu vitae sagittis. </p>
+        </div>
+    );
+}
