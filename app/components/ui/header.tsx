a99aa8890e594559737918b5cfa17fb673cd1ae6
ldaws003 <leondawson123@yahoo.com>
2026-06-06 00:14:47 -0400

    added logo and responsive css

--- app/components/ui/header.tsx
+++ app/components/ui/header.tsx
@@ -1,12 +1,14 @@
 import { auth, signIn, signOut } from '../../../src/auth';
 import Image from 'next/image';
 import NavBar from './NavBar';
+import { CiDumbbell } from "react-icons/ci";
 import Link from 'next/link';
 
 
 export default function Header(){
   return (
-    <header>
+    <header className="bg-emerald-800 px-5 py-5 sticky w-full start-0 border-b border-default flex flex-nowrap sm:flex-wrap justify-between items-center">
+      <CiDumbbell size={32}/>
       <NavBar />
     </header>
   )
