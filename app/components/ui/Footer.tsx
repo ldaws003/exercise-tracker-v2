273608f344a2d75a8c5271bc58c37192d16d80cd
ldaws003 <leondawson123@yahoo.com>
2026-06-06 00:14:32 -0400

    added links and css

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -1,9 +1,13 @@
 //TODO: finish footer
+import Link from "next/link";
 
 export default function Footer(){
     return (
-        <footer>
-            <p>Hello World</p>
+        <footer className="bg-emerald-800 px-5 py-5 w-full flex flex-nowrap sm:flex-wrap gap-x-3 flex-row items-start">
+            <Link className="inline hover:bg-emerald-950 p-1 mx-5 text-center" href="/about">About</Link>
+            <Link className="inline hover:bg-emerald-950 p-1 mx-5 text-center" href="/contact-us">Contact</Link>
+            <a className="inline hover:bg-emerald-950 p-1  mx-5 text-center" href="https://portfolio-leon.appwrite.network/" target="_blank" rel="noopener noreferrer">Portfolio</a>
+            <a className="inline hover:bg-emerald-950 p-1  mx-5 text-center" href="https://github.com/ldaws003/exercise-tracker-v2" target="_blank" rel="noopener noreferrer">GitHub</a>
         </footer>
     );
 }
