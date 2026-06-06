61137daf60903d9c563ed8f7b055accc5d6428ae
ldaws003 <leondawson123@yahoo.com>
2026-06-06 00:31:00 -0400

    added css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,8 +2,8 @@
 
 export default function About(){
     return (
-        <div className="bg-slate-100 text-black">
-            <h1>About Me</h1>
+        <div className="bg-slate-100 self-auto text-black self-start max-w-2xl mx-auto">
+            <h1 className="text-5xl my-4">About Me</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus aliquam viverra. Cras eget ex nec ligula 
                 porttitor tincidunt. Duis rhoncus diam imperdiet odio ullamcorper, eu imperdiet libero scelerisque. Cras 
                 efficitur placerat pulvinar. Donec sed libero a dolor feugiat feugiat. Vivamus gravida ligula quis arcu bibendum
--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,7 +1,7 @@
 export default function Contact(){
     return (
-        <div className="bg-slate-100 text-black">
-            <h1>Contact Us</h1>
+        <div className="bg-slate-100 self-auto text-black self-start max-w-2xl mx-auto">
+            <h1 className="text-5xl my-4">Contact Us</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus aliquam viverra. Cras eget ex nec 
                 ligula porttitor tincidunt. Duis rhoncus diam imperdiet odio ullamcorper, eu imperdiet libero scelerisque. 
                 Cras efficitur placerat pulvinar. Donec sed libero a dolor feugiat feugiat. Vivamus gravida ligula quis arcu 
